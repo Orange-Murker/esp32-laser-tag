e0dc79fb1d8b1860e87bdb10f8d2dd4befd9a8f5
imnooneshawty <a.arlauskas@student.utwente.nl>
2022-10-20 23:40:24 +0200

    Added shooting, getting shot and dying animations

--- esp32/src/feedback_to_the_user.cpp
+++ esp32/src/feedback_to_the_user.cpp
@@ -10,14 +10,29 @@ CRGB leds[NUM_LEDS];
 unsigned long vibrate_until = 0;
 bool vibrating = false;
 
-unsigned long show_leds_until = 0;
 const CRGB black = CRGB::Black;
 CRGB led_show_colour = CRGB::Black;
+
+const unsigned long animation_delay = 700;
+const unsigned long ms_per_led = animation_delay / NUM_LEDS;
+unsigned long show_led_until = 0;
+
+boolean shooting = false;
+int shoot_anim_index = 0;
+
+boolean shot = false;
+boolean dead = false;
+boolean death_animation = false;
+const unsigned long ms_per_led_dead = 10 * ms_per_led;
+unsigned long shot_until = 0;
+unsigned int led_death_decay = 255 / NUM_LEDS;
+int dead_anim_index = 9;
 
 uint8_t health = 255;
 
 void fill_leds_solid_colour(CRGB colour) {
-    for (int i = 0; i < NUM_LEDS; i++) {
+    for (int i = 0; i < NUM_LEDS; i++)
+    {
         leds[i] = colour;
     }
     FastLED.show();
@@ -25,7 +40,7 @@ void fill_leds_solid_colour(CRGB colour) {
 
 void show_health() {
     uint8_t leds_to_show = (health * NUM_LEDS) / MAX_HEALTH;
-    
+
     // Fill the LEDs with a healthy colour
     for (int i = 0; i < leds_to_show; i++) {
         leds[i] = CRGB::Green;
@@ -33,7 +48,7 @@ void show_health() {
 
     // Fill the LEDs with an unhealthy colour
     for (int i = leds_to_show; i < NUM_LEDS; i++) {
-        leds[i] = CRGB::Black;
+        leds[i] = black;
     }
     FastLED.setBrightness(50);
 
@@ -41,6 +56,7 @@ void show_health() {
 }
 
 void initialise_feedback() {
+
     pinMode(VIBRATOR_PIN, OUTPUT);
     digitalWrite(VIBRATOR_PIN, LOW);
 
@@ -48,8 +64,40 @@ void initialise_feedback() {
     show_health();
 }
 
-
 void feedback_update() {
+    if(shot) {
+        if(millis() > shot_until) {
+            if (health > 0) {
+                shot = false;
+                show_health();
+            }
+            else {
+                if (dead_anim_index >= 0) {
+                    if ((millis() <= show_led_until)
+                    && !death_animation) {
+                        FastLED.setBrightness(FastLED.getBrightness() - led_death_decay); 
+                        leds[dead_anim_index] = black;
+                        FastLED.show();
+                        death_animation = true;
+                    }
+                    else {
+                        death_animation = false;
+                        show_led_until = show_led_until + ms_per_led_dead;
+                        dead_anim_index--;
+                    }
+                }
+                else {
+                    fill_leds_solid_colour(CRGB::Red);
+                    FastLED.show();
+                    shot = false;
+                // useless unless respawns implemented:
+                    // dead_anim_index = NUM_LEDS - 1;
+                    // show_led_until = 0;
+                }
+            }
+        }
+    }
+
     if (millis() > vibrate_until) {
         if (vibrating) {
             digitalWrite(VIBRATOR_PIN, LOW);
@@ -57,33 +105,62 @@ void feedback_update() {
         }
     }
 
-    if (millis() > show_leds_until) {
-        if (led_show_colour != black) {
-            led_show_colour = CRGB::Black;
+    if (shooting) {
+        if (((millis() <= show_led_until)|| shoot_anim_index == 0)
+        && shoot_anim_index != NUM_LEDS) {
+            if (shoot_anim_index != 0) {
+                leds[shoot_anim_index - 1] = black;
+            }
 
-            // Return to displaying the health after a flash
+            leds[shoot_anim_index] = CRGB::Yellow;
+            shoot_anim_index++;
+            show_led_until = millis() + ms_per_led;
+
+            FastLED.show();
+        }
+        else {
+            leds[NUM_LEDS - 1] = black;
+            FastLED.show();
+            shoot_anim_index = 0;
+            show_led_until = 0;
             show_health();
+            shooting = false;
         }
     }
-} 
+}
 
 void trigger_pressed_feedback() {
-    digitalWrite(VIBRATOR_PIN, HIGH);
-    vibrating = true;
-    vibrate_until = millis() + 80;
-
-    led_show_colour = CRGB::Yellow;
-    FastLED.setBrightness(255);
-    fill_leds_solid_colour(led_show_colour);
-    show_leds_until = millis() + 80;
+    if (!dead) {
+        digitalWrite(VIBRATOR_PIN, HIGH);
+        vibrating = true;
+        vibrate_until = millis() + 20;
+        
+        FastLED.setBrightness(255);
+        fill_leds_solid_colour(black);
+        shoot_anim_index = 0;
+        shooting = true;
+    }
 }
 
 void got_shot_feedback(uint8_t current_health) {
     health = current_health;
 
-    digitalWrite(VIBRATOR_PIN, HIGH);
-    vibrating = true;
-    vibrate_until = millis() + 150;
+    if (!dead) {
+        if (health <= 0) {
 
-    show_health();
+            dead = true;
+
+        }
+
+        digitalWrite(VIBRATOR_PIN, HIGH);
+        vibrating = true;
+        vibrate_until = millis() + 150;
+
+        FastLED.setBrightness(255);
+        fill_leds_solid_colour(CRGB::Red);
+        FastLED.show();
+        shot = true;
+        shot_until = millis() + ms_per_led;
+        show_led_until = millis() + ms_per_led + 10;
+    }
 }

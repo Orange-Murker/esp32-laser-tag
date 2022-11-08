5fe8143ef8b25c65c185c7fdc59ef007efbff776
Jetse Verschuren <j.verschuren@student.utwente.nl>
2022-11-08 15:07:03 +0100

    Button styling

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -9,7 +9,7 @@ export default function Button({
   return (
     <button
       className={
-        "border border-black rounded-xl bg-white py-2 px-3 text-slate-800 " +
+        "border border-black rounded-xl bg-white bg-opacity-75 py-2 px-3 text-slate-800 " +
           className ?? ""
       }
       {...rest}
